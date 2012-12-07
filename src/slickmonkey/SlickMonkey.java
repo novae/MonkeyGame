c5d4c45fbf855abc1a5e23359dafba211e2de10e
novaegr <novaegr@gmail.com>
2012-12-07 03:22:45 -0600

    correct asstes

--- src/slickmonkey/SlickMonkey.java
+++ src/slickmonkey/SlickMonkey.java
@@ -34,7 +34,7 @@ public class SlickMonkey extends BasicGame{
     public void init(GameContainer gc) throws SlickException {
        gc.setVSync(true);
        sheet = new SpriteSheet("resources/monkey.png",30,30);
-       map = new TiledMap("resources/map.tmx");
+       map = new TiledMap("resources/mapa.tmx");
        Monkey = new Animation();
        Monkey.setAutoUpdate(true);
        moveMonkey(steps,0);
