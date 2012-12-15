c812e57cb38be74639c1ddabc49473bee8cab34e
novaegr <novaegr@gmail.com>
2012-12-14 19:50:36 -0600

    collision Class new

--- src/slickmonkey/SlickMonkey.java
+++ src/slickmonkey/SlickMonkey.java
@@ -1,20 +1,16 @@
-
 package slickmonkey;
-
-
-import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class SlickMonkey extends BasicGame{
     private TiledMap map;
-    private  float monkeyX,monkeyY=150;
+    private  float monkeyX=150,monkeyY=150;
+    private Rectangle monkeyRect;
     private Animation Monkey;
     SpriteSheet sheet;
     int [] steps={0,1,2};
-    private Polygon MonkeyPolygon;
-    public BlockMap mapblock;
-    
+  
     public SlickMonkey(String title) throws SlickException {
      super(title);
     }
@@ -34,87 +30,50 @@ public class SlickMonkey extends BasicGame{
     public void init(GameContainer gc) throws SlickException {
        gc.setVSync(true);
        sheet = new SpriteSheet("resources/monkey.png",30,30);
-       //map = new TiledMap("resources/mapa.tmx");
-       mapblock = new BlockMap("resources/mapa.tmx");
+       map = new TiledMap("resources/mapa.tmx");
        Monkey = new Animation();
        Monkey.setAutoUpdate(true);
+       monkeyRect=new Rectangle(monkeyX,monkeyY,sheet.getWidth(),sheet.getHeight());
        moveMonkey(steps,0);
-       
-       MonkeyPolygon = new Polygon(new float[]{
-				monkeyX,monkeyY,
-				monkeyX+30,monkeyY,
-				monkeyX+30,monkeyY+30,
-				monkeyX,monkeyY+30
-		});
-       
     }
     
     public void moveMonkey(int [] steps,int y){
         for (int frame=0; frame < steps.length-1 ;frame++){
             Monkey.addFrame(sheet.getSprite(steps[frame],y),150);
         }
-        
+        monkeyRect.setX(monkeyX);
+        monkeyRect.setY(monkeyY);
     }
     
-    public boolean entityCollisionWith()throws SlickException{
-        for(int i = 0; i < BlockMap.entities.size(); i ++){
-            Block entity1 = (Block)BlockMap.entities.get(i);
-            if(MonkeyPolygon.intersects(entity1.poly)){
-                return true;
-            }
-        }
-        return false;
-    }
 
-    @Override
-    public void update(GameContainer gc, int i) throws SlickException {
+    public void updateCharacter(GameContainer gc)throws SlickException{
         if (gc.getInput().isKeyDown(Input.KEY_LEFT)) {
             monkeyX--;
             moveMonkey(steps,1);
-            MonkeyPolygon.setX(monkeyX);
-            if(entityCollisionWith()){
-                monkeyX++;
-                MonkeyPolygon.setX(monkeyX);
-            }
         }
-	if (gc.getInput().isKeyDown(Input.KEY_RIGHT)) {
+        if (gc.getInput().isKeyDown(Input.KEY_RIGHT)) {
             monkeyX++;
             moveMonkey(steps,2);
-            MonkeyPolygon.setX(monkeyX);
-            if(entityCollisionWith()){
-                monkeyX--;
-                MonkeyPolygon.setX(monkeyX);
-            }
-            
         }
-	if (gc.getInput().isKeyDown(Input.KEY_UP)) {
+        if (gc.getInput().isKeyDown(Input.KEY_UP)) {
             monkeyY--;
             moveMonkey(steps,3);
-            MonkeyPolygon.setY(monkeyY);
-            if(entityCollisionWith()){
-                monkeyX++;
-                MonkeyPolygon.setY(monkeyY);
-            }
         }
-	if (gc.getInput().isKeyDown(Input.KEY_DOWN)) {
+        if (gc.getInput().isKeyDown(Input.KEY_DOWN)) {
             monkeyY++;
             moveMonkey(steps,0);
-            MonkeyPolygon.setY(monkeyY);
-            if(entityCollisionWith()){
-                monkeyX--;
-                MonkeyPolygon.setY(monkeyY);
-            }
         }
+        
     }
-    
-   
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+        updateCharacter(gc);
+}
 
     @Override
     public void render(GameContainer gc, Graphics grphcs) throws SlickException {
-        BlockMap.tmap.render(0, 0);
+        map.render(0, 0);
         grphcs.drawAnimation(Monkey, monkeyX, monkeyY);
-        grphcs.draw(MonkeyPolygon);
+        grphcs.drawRect(monkeyRect.getX(),monkeyRect.getY(),30,30);
     }
-    
-    
 }
